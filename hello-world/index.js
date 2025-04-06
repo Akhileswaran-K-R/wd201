ac456c6fc7d24635a9a447d441b6b5a1d6f4e792
Akhileswaran-K-R <akhilarathi956@gmail.com>
2025-04-06 07:10:53 +0530

    learned npm install

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,44 @@
-const hello = () => {
-  console.log("Hello GitHub!");
-};
+// const fs = require("fs");
+// fs.writeFile(
+//   "sample.txt",
+//   "Hello World! Welcome to node.js File system module",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File created");
+//   }
+// );
 
-hello();
+// fs.readFile("sample.txt", (err, data) => {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
+
+// fs.appendFile("sample.txt", " This is updated", (err) => {
+//   if (err) throw err;
+//   console.log("Updated file");
+// });
+
+// fs.rename("sample.txt", "test.txt", (err) => {
+//   if (err) throw err;
+//   console.log("Rename successfull");
+// });
+
+// //deletion
+// fs.unlink("test.txt", (err) => {
+//   if (err) throw err;
+//   console.log("Deletion successful");
+// });
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const stream = fs.createReadStream("sample.txt");
+  stream.pipe(res);
+  // fs.readFile("sample.txt", (err, data) => {
+  //   if (err) throw err;
+  //   res.end(data);
+  // });
+});
+
+server.listen(3000);
